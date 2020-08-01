e9ccabee0b42adb47e43f6c906e2318b0cb0a2e2
EvandroChucky <evandrochucky@gmail.com>
2020-07-31 22:56:16 -0300

    Treatment of Commit and Rollback

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,10 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 import db.DB;
+import db.DbException;
 import db.DeleteData;
 import db.InsertData;
 import db.UpdateData;
@@ -13,21 +15,43 @@ public class Program {
 
 		Connection conn= null;
 
-		conn = DB.getConnection();
+		try {
+			
+			conn = DB.getConnection();
+			
+			conn.setAutoCommit(false);
+			// Setamos para FALSE para controlar o commit para a transação completa.
+			// Só vai commitar no banco quando terminar todas as execuções em tabelas
+			// Caso contrário, executamos rollback na transação
+			
+			InsertData.insertSeller(conn, "Paulo", "paulo@gmail.com", "23/11/1976", 3500.30, 4);
+	
+			InsertData.insertDepartment(conn, "Dept11", "Dept12");
+			
+			
+			UpdateData.updateSeller(conn, 200.00, 2);
+	
+			UpdateData.updateDepartment(conn, 23, "Natural Parks");
+			
+			DeleteData.deleteSeller(conn, 11);
+			DeleteData.deleteDepartment(conn, 25);
 
-		
-		InsertData.insertSeller(conn, "Paulo", "paulo@gmail.com", "23/11/1976", 3500.30, 4);
+			//As alterações no banco de dados serão efetivadas somente no final do processo, garantindo integridade relacional
+			conn.commit();
+			
+		}
+		catch (SQLException e){
+			try {
+				conn.rollback();
+				throw new DbException("Transaction Rolled back! Caused by: " + e.getMessage());
+			} 
+			catch (SQLException e1) {
+				throw new DbException("Error trying rollback! Caused by: " + e1.getMessage());
+			}
+		}
+		finally {
+			DB.closeConnection();
+		}
 
-		InsertData.insertDepartment(conn, "Dept11", "Dept12");
-		
-		
-		UpdateData.updateSeller(conn, 200.00, 2);
-
-		UpdateData.updateDepartment(conn, 16, "Natural");
-		
-		DeleteData.deleteSeller(conn, 10);
-		DeleteData.deleteDepartment(conn, 2);
-
-		DB.closeConnection();
 	}
 }
